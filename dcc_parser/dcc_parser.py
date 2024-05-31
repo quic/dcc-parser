a8b554a294f93b3e0571345231efd4971007c08d
Mark Matyas <quic_mmatyas@quicinc.com>
2024-05-31 14:48:04 -0700

    Re-arrange copyright header and spdx license identifier
    Signed-off-by: Mark Matyas <quic_mmatyas@quicinc.com>

--- dcc_parser/dcc_parser.py
+++ dcc_parser/dcc_parser.py
@@ -1,6 +1,5 @@
-# SPDX-License-Identifier: GPL-2.0-only
-# Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 # Copyright (c) 2015, 2017, 2019-2021 The Linux Foundation. All rights reserved.
+# Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -10,6 +9,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+# SPDX-License-Identifier: GPL-2.0-only
 
 import datetime
 import logging
